637607e47a265681fe1663d6f3a7975b2db2563d
talhaty <talhayounas0348@gmail.com>
2023-07-05 21:21:49 +0500

    dashboard

---
+++ frontend/src/layouts/dashboard/config.js
@@ -0,0 +1,59 @@
+import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
+import CogIcon from '@heroicons/react/24/solid/CogIcon';
+import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
+import Bolt from '@heroicons/react/24/solid/BoltIcon';
+import UserIcon from '@heroicons/react/24/solid/UserIcon';
+import UserPlusIcon from '@heroicons/react/24/solid/UserPlusIcon';
+import UsersIcon from '@heroicons/react/24/solid/UsersIcon';
+import XCircleIcon from '@heroicons/react/24/solid/XCircleIcon';
+import { SvgIcon } from '@mui/material';
+
+export const items = [
+  {
+    title: 'Overview',
+    path: '/',
+    icon: (
+      <SvgIcon fontSize="small">
+        <ChartBarIcon />
+      </SvgIcon>
+    )
+  },
+  
+  {
+    title: 'Customers',
+    path: '/customers',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UsersIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Chat Bot',
+    path: '/chatbot',
+    icon: (
+      <SvgIcon fontSize="small">
+        <Bolt/>
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Account',
+    path: '/account',
+    icon: (
+      <SvgIcon fontSize="small">
+        <UserIcon />
+      </SvgIcon>
+    )
+  },
+  {
+    title: 'Settings',
+    path: '/settings',
+    icon: (
+      <SvgIcon fontSize="small">
+        <CogIcon />
+      </SvgIcon>
+    )
+  }
+ 
+];
