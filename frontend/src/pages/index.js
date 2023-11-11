5806e1201c8ed7289d63fa94ffd9eb2994a22462
talhaty <talhayounas0348@gmail.com>
2023-11-12 02:02:45 +0500

    updated main page

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,18 +1,42 @@
 import Head from "next/head";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
-const apiUrl = "http://localhost:5000";
-
+import { Container, Typography, Paper, Grid, Card, CardContent } from "@mui/material";
 const now = new Date();
 
 const Page = () => {
   return (
     <>
-    <Head>
-      <title>
-        Overview | Keystone
-      </title>
-    </Head>
-  </>
+      <Head>
+        <title>Overview | Keystone</title>
+      </Head>
+      <Container maxWidth="lg" style={{ paddingTop: 20 }}>
+        <Typography variant="h4" gutterBottom align="center" marginBottom={5}>
+          Welcome to Keystone Dashboard
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" align="center" gutterBottom>
+                  Favourite Charts
+                </Typography>
+                
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6" align="center" gutterBottom>
+                  Recent Activity
+                </Typography>
+               
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
